19c083f888c7fc22945a0b94455a7e6e1db51d7f
Edivan <edivanlima23@gmail.com>
2021-02-14 10:50:26 -0300

    testando console

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -8,7 +8,6 @@ cliente1.cpf = 11122233309;
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -22,7 +21,7 @@ conta2.agencia = 102;
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log("valor: ", valor)
+console.log("valor: ", valor);
 console.log(conta2);
 
-
+console.log(ContaCorrente);
